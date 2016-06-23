1d927f28a38efddb97474750dacdf17cb8736bae
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-06-23 16:07:34 -0400

    Update wrap_dev.py

--- wrap_dev.py
+++ wrap_dev.py
@@ -5,23 +5,30 @@ import nipype.interfaces.io as nio
 
 eng = matlab.engine.start_matlab()
 
-def setup(vbvs, gpml, depvb, outpath):
-    import os
-    for i in [vbvs, gpml, depvb]:
-        eng.addpath(i)
-    if gpml[-1] == '/':
-        eng.run(gpml+'startup.m', nargout=0)
+
+def checkstr(string):
+    if string[-1] == '/':
+        return string
     else:
-        eng.run(gpml+'/startup.m', nargout=0)
+        return string + '/'
+
+def setup(vbvs, gpml, depvb, comp, outpath):
+    import os
+    for i in [vbvs, gpml, depvb, comp]:
+        eng.addpath(i)
+    eng.run(checkstr(outpath), nargout=0)
     os.chdir(outpath)
+    return output
+    
+Setup = pe.Node(name='Setup',
+                interface=Function(input_names=['vbvs','gpml',
+                                                'depvb','comp','outpath'],
+                                        output_names=['outpath'],
+                                        function=setup))
 
 def csv(colnums, matfiles):
     import pandas as pd
-    df = pd.DataFrame()
-    df.columns = ['colnum', 'matFn']
-    for i in range(len(colnums)):
-        df.ix[i] = [i, matfiles[i]]
-    df.to_csv('BFResultsFileList.csv')
-    
-def runsteps(step, infile, outfile, data, colnum):
-    # next
+    df = pd.DataFrame(columns=['colNum','matFn'])
+    for i in range(colnums):
+        df.loc[i] = [i, matfiles[i]]
+    return df 
