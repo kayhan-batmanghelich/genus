fb41369352c69aa6829d099d98d4c0c1666a12fb
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-06-23 00:19:34 -0400

    Update wrap.py

--- wrap.py
+++ wrap.py
@@ -16,4 +16,4 @@ eng.addpath('/om/user/ysa/genus/bayes/Carbonetto_VBS/MATLAB')
 eng.addpath('/om/user/ysa/genus/bayes/basis/bayesianImagingGenetics/src')
 data = '/om/user/ysa/genus/adni/natureImput-ChrAll_Data94.mat'
 os.chdir('/om/user/ysa/testdir/')
-eng.deployEndoPhenVB('step','bf','inputMat',data,'colNum',2,'outFile','t_test_2_m.m', nargout=0)
+eng.deployEndoPhenVB('step','bf','inputMat',data,'colNum',2,'outFile','test_2.m', nargout=0)
