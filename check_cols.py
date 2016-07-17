1a5c005bcbfe9775e126800a950d08ef89d4d0da
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-07-16 20:20:51 -0400

    Update check_cols.py

--- check_cols.py
+++ check_cols.py
@@ -6,6 +6,10 @@ data = scipy.io.loadmat('scz_eur_pgc_annealed.mat')
 I = data['I']
 
 def lin_dep(data):
+    '''
+    ind_cols: linearly independent columns(features)
+    dep_cols: linearly dependent columns
+    '''
     R = np.linalg.qr(data)[1]
     ind_cols = np.where(np.abs(R.diagonal()) > 0)[0]
     cols = np.array([x for x in range(data.shape[1])])
