82e07f0ebba180c5ea864d502b0a1a5391381d60
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-07-20 15:31:49 -0400

    Update cca.py

--- cca.py
+++ cca.py
@@ -11,7 +11,7 @@ class cca(object):
         self.cca_type = cca_type
         
     
-    def lin_dep(data):
+    def lin_dep(self, data):
         R = np.linalg.qr(data)[1]
         ind_cols = np.where(np.abs(R.diagonal()) > 0)[0]
         cols = np.array([x for x in range(data.shape[1])])
@@ -21,15 +21,20 @@ class cca(object):
     def val_args(self, X, Y):
         X_row, X_col = X.shape
         Y_col = Y.shape[1]
+        return [(X_row, X_col), Y_col]
         
     def demean(self, X, Y):
         X = X - X.mean(0)
         Y = Y - Y.mean(0)
         return X, Y
     
-    def rank(self, X, Y):
+    def decomp(self, X, Y):
         q_x, r_x, p_x = linalg.qr(X, mode='economic', pivoting=True)
         q_y, r_y, p_y = linalg.qr(Y, mode='economic', pivoting=True)
+        return [(q_x, r_x, p_x), (q_y, r_y, p_y)]
+    
+    def rank(self, X, Y):
         rank_x = np.linalg.matrix_rank(r_x)
         rank_y = np.linalg.matrix_rank(r_y)
+        return rank_x, rank_y
         
