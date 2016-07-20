06b45f1d8c94ff7191f6e277e403f9e829e5c93d
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-07-20 10:44:26 -0400

    Update cca.py

--- cca.py
+++ cca.py
@@ -11,3 +11,22 @@ def lin_dep(data):
     dep_cols = sorted(list(set(cols) - set(ind_cols)))
     return data[:, ind_cols], dep_cols
     
+
+X = np.array([np.random.normal(size=3) for x in range(60)]).T
+Y = np.array([np.random.normal(size=3) for x in range(60)]).T
+
+
+'''
+rowvar: default - True
+True: row are variables, columns are observations
+False: rows are observations, columns are variables
+
+
+This does not work as you cannot multiply:
+covarx**(-1/2) * covarxy * covary(-1/2)
+'''
+covarx = np.cov(X, rowvar=False)
+covary = np.cov(Y, rowvar=False)
+covarxy = np.cov(X,Y, rowvar=False)
+
+
