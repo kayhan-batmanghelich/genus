966e92045ce0c2d46b1bf46d0e52af5d8831402d
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-10-11 13:39:54 -0400

    Update lasso_cv.R

--- lasso_cv.R
+++ lasso_cv.R
@@ -12,42 +12,48 @@ if (dim(X)[1] != length(y)) {
     print("X and y have the same number of samples")
 }
 
-Flds <- createFolds(y, k=5, list=TRUE)
-models_auc <- list()
-models_class <- list()
-auc_best_lambdas <- c()
-class_best_lambdas <- c()
+auc_accs <- c()
+class_accs <- c()
 
-fold_vars <- sprintf("Flds$Fold%01d", seq(5))
+for (i in 1:30) {
+    Flds <- createFolds(y, k=5, list=TRUE)
+    models_auc <- list()
+    models_class <- list()
+    auc_best_lambdas <- c()
+    class_best_lambdas <- c()
 
-# compute models
-for (i in seq(3)) {
-    models_auc[[i]] <- cv.glmnet(X[eval(parse(text=fold_vars[i])),],
-                     y[eval(parse(text=fold_vars[i]))],
-                     family='binomial',
-                     type.measure='auc',
-                     nfolds=4)
+    fold_vars <- sprintf("Flds$Fold%01d", seq(5))
 
-   models_class[[i]] <- cv.glmnet(X[eval(parse(text=fold_vars[i])),],
-                     y[eval(parse(text=fold_vars[i]))],
-                     family='binomial',
-                     type.measure='class',
-                     nfolds=4)
+    # compute models
+    for (i in seq(3)) {
+        models_auc[[i]] <- cv.glmnet(X[eval(parse(text=fold_vars[i])),],
+                         y[eval(parse(text=fold_vars[i]))],
+                         family='binomial',
+                         type.measure='auc',
+                         nfolds=4)
+
+       models_class[[i]] <- cv.glmnet(X[eval(parse(text=fold_vars[i])),],
+                         y[eval(parse(text=fold_vars[i]))],
+                         family='binomial',
+                         type.measure='class',
+                         nfolds=4)
+    }
+
+    # get the bestscores and lambdas
+    for (i in seq(3)) {
+      auc_best_lambdas[i] <- models_auc[[i]]$lambda.min
+      class_best_lambdas[i] <- models_class[[i]]$lambda.min
+    }
+
+    # cross-validate on the left out fold using lambdas from 4 models computed
+    mod_auc_4 <- cv.glmnet(X[Flds$Fold4,], y[Flds$Fold4], family='binomial', type.measure='auc', lambda=auc_best_lambdas)
+    mod_class_4 <- cv.glmnet(X[Flds$Fold4,], y[Flds$Fold4], family='binomial', type.measure='class', lambda=class_best_lambdas)
+
+    # final test set
+    auc_pred <- predict(mod_auc_4, newx= X[Flds$Fold5,],  type="class")
+    auc_accs[i] <- sum(as.numeric(auc_pred) == y[Flds$Fold5]) / length(y[Flds$Fold5])
+
+    class_pred <- predict(mod_class_4, newx= X[Flds$Fold5,],  type="class")
+    class_accs[i] <- sum(as.numeric(class_pred) == y[Flds$Fold5]) / length(y[Flds$Fold5])
+    
 }
-
-# get the bestscores and lambdas
-for (i in seq(3)) {
-  auc_best_lambdas[i] <- models_auc[[i]]$lambda.min
-  class_best_lambdas[i] <- models_class[[i]]$lambda.min
-}
-
-# cross-validate on the left out fold using lambdas from 3 models computed
-mod_auc_4 <- cv.glmnet(X[Flds$Fold4,], y[Flds$Fold4], family='binomial', type.measure='auc', lambda=auc_best_lambdas)
-mod_class_4 <- cv.glmnet(X[Flds$Fold4,], y[Flds$Fold4], family='binomial', type.measure='class', lambda=class_best_lambdas)
-
-# final test set
-auc_pred <- predict(mod_auc_4, newx= X[Flds$Fold5,],  type="class")
-auc_acc <- sum(as.numeric(auc_pred) == y[Flds$Fold5]) / length(y[Flds$Fold5])
-
-class_pred <- predict(mod_class_4, newx= X[Flds$Fold5,],  type="class")
-class_acc <- sum(as.numeric(class_pred) == y[Flds$Fold5]) / length(y[Flds$Fold5])
