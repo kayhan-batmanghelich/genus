a6a495e38f2799d1204b78985e7824442d1c785d
YSanchezAraujo <yoelsanchez006@gmail.com>
2016-10-10 23:57:03 -0400

    Create lasso_cv.R

---
+++ lasso_cv.R
@@ -0,0 +1,19 @@
+setwd('/data/petryshen/yoel/lasso')
+require(caret)
+require(glmnet)
+
+X <- read.csv('endo_mat_1035_170.csv')
+X <- as.matrix(sapply(X, as.matrix))
+y <- read.csv('reduced_y.txt', header=FALSE)
+y <- as.vector(as.matrix(y))
+
+if (dim(X)[1] != length(y)) {
+    for (i in 1:10) {
+        print("X and y do not have the same number of samples")
+    }
+} else {
+    print("X and y have the same number of samples")
+}
+
+# performs stratified k fold 
+F <- createFolds(y, k=10, list=TRUE)
